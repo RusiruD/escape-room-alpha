fea72243a35cc49821c5b0c953b11f25603befde
Rusiru <rdha868@aucklanduni.ac.nz>
2023-07-30 09:47:59 +1200

    added files and added a riddle, start button and other buttons

--- src/main/java/nz/ac/auckland/se206/GameState.java
+++ src/main/java/nz/ac/auckland/se206/GameState.java
@@ -6,6 +6,9 @@ public class GameState {
   /** Indicates whether the riddle has been resolved. */
   public static boolean isRiddleResolved = false;
 
+  public static boolean isRiddle1Resolved = false;
+  public static boolean isSafeOpen = false;
+
   /** Indicates whether the key has been found. */
   public static boolean isKeyFound = false;
 }
